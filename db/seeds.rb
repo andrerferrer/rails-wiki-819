d7477a9290604f4a7d7eca602d242e2cd65e8765
Andre Ferrer <andrerferrer@hotmail.com>
2022-02-28 15:26:01 +0100

    improve seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,17 @@
+User.create!(
+  email: 'player@one.com',
+  password: 123456
+)
+
 first_post = Post.create
 
-Section.create!(text: "First Paragraph", post: first_post)
-Section.create!(text: "Second Paragraph", post: first_post)
+lorem = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum itaque veritatis asperiores facere accusantium libero corrupti dolorum consectetur quam fuga, rem consequatur molestias eligendi, numquam culpa, consequuntur magni beatae ducimus?
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum itaque veritatis asperiores facere accusantium libero corrupti dolorum consectetur quam fuga, rem consequatur molestias eligendi, numquam culpa, consequuntur magni beatae ducimus?
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum itaque veritatis asperiores facere accusantium libero corrupti dolorum consectetur quam fuga, rem consequatur molestias eligendi, numquam culpa, consequuntur magni beatae ducimus?
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum itaque veritatis asperiores facere accusantium libero corrupti dolorum consectetur quam fuga, rem consequatur molestias eligendi, numquam culpa, consequuntur magni beatae ducimus?
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum itaque veritatis asperiores facere accusantium libero corrupti dolorum consectetur quam fuga, rem consequatur molestias eligendi, numquam culpa, consequuntur magni beatae ducimus?"
+
+Section.create!(text: "First Paragraph #{lorem}", post: first_post)
+Section.create!(text: "Second Paragraph #{lorem}", post: first_post)
 
 EditRequest.create!(new_text: "Very Nice First Paragraph", section: Section.first)
